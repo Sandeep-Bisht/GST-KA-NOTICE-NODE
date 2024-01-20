d8f781fdb6556ee5c877b5606779678a8998e99e
Sandeep Bisht <>
2024-01-20 18:53:56 +0530

    feat create profile API

---
+++ controllers/users/index.js
@@ -0,0 +1,85 @@
+const Profile = require("../../models/profile");
+const UserRoles =require('../../models/user_roles')
+
+exports.setProfile = async (req, res) => {
+    try{
+        // delete data.image;
+        let data = { ...req.body, featuredIcon: req.files[0]};
+
+        updateProfile({user:req.user,profile:data}).then((response)=>{
+            return res.status(response.status).json(response);
+        })
+        .catch((error)=>{
+          res.status(400).json({
+            error:true,
+            errorMessage:error.message,
+            message: "Error updating profile.",
+          });
+        });
+
+        
+    }
+    catch(err){
+        res.status(500).json({
+            error:true,
+            message:"Something went wrong please try again later."
+        })
+    }
+}
+
+exports.getProfile = async (req, res) => {
+    try{
+        const existingProfile = await Profile.findOne({'user_id':req.user._id});
+        console.log("inside existingProfile", existingProfile)
+        const role = await UserRoles.findOne({'user_id':req.user._id});       
+        return res.send({...existingProfile._doc,role:role.role_id});
+    }
+    catch(err){
+        res.status(500).json({
+            error:true,
+            message:"Something went wrong please try again later."
+        })
+    }
+}
+
+
+const updateProfile = async (data) => {
+    try {
+  
+      const { user, profile } = data;
+  
+      const existingProfile = await Profile.findOne({'user_id':user._id});
+  
+      if(!existingProfile){
+        return {
+          error: true,
+          status: 500,
+          message: 'User not found in profile.',
+        };
+      }
+  
+      const profileResult = await Profile.findOneAndUpdate({_id:existingProfile._id},{$set:profile}, { new: true });
+  
+      if (!profileResult) {
+        return {
+          error: true,
+          status: 500,
+          message: 'Error updating profile.',
+        };
+      }
+  
+      return {
+        error: false,
+        status: 200,
+        message: 'profile updated successfully!',
+      };
+      
+    } catch (error) {
+      return {
+        error: true,
+        errorMessage:error.message,
+        status: 500,
+        message: 'Error updating profile.',
+      };
+    }
+  };
--- controllers/users/routes.js
+++ controllers/users/routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router()
 
 const { createTicket } = require('./tickets')
+const { getProfile, setProfile } = require('./index')
 const passport = require("passport")
 const upload = require('../../config/multer')
 
-router.post('/create-ticket',passport.authenticate('jwt', {session:false}), upload.fields([{name: 'document'}]), createTicket)
+router.post('/create-ticket',passport.authenticate('jwt', {session:false}), upload.fields([{name: 'document'}]), createTicket);
+router.post('/setProfile',passport.authenticate('jwt',{session:false}),upload.any('files'),setProfile)
+router.get('/getProfile',passport.authenticate('jwt',{session:false}),getProfile)
 
 module.exports = router
