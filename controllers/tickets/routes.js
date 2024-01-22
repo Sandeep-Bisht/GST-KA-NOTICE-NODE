3c4488db0a3a4ccf3d48b2c9a4b049b36a1fc586
SHUBHAM-SEMWAL <87406338+SHUBHAM-SEMWAL@users.noreply.github.com>
2024-01-22 19:35:29 +0530

    create tickets api

--- controllers/tickets/routes.js
+++ controllers/tickets/routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router()
 
-const { getAllTickets, getByTicketNo, askForPayment} = require('./ticket')
+const { getAllTickets, getByTicketNo, askForPayment, uploadAskedDocs} = require('./ticket')
 const passport = require("passport")
+const upload = require('../../config/multer')
 
 router.get('/get-all-tickets',passport.authenticate('jwt', {session:false}),getAllTickets)
 
 router.get('/get-by-ticketNo/:ticketNo',passport.authenticate('jwt', {session:false}),getByTicketNo)
 router.post('/ask-payment/:ticketNo',passport.authenticate('jwt', {session:false}),askForPayment)
+router.post('/upload-asked-documents/:ticketNo',passport.authenticate('jwt', {session:false}), upload.any('files'), uploadAskedDocs)
 
 module.exports = router
--- controllers/tickets/ticket.js
+++ controllers/tickets/ticket.js
@@ -112,3 +112,71 @@ exports.askForPayment = async (req, res) => {
       })
     }
 }
+
+exports.uploadAskedDocs = async (req, res) => {
+  try{
+      let {ticketNo} = {...req.params};
+      let {title} = {...req.body}
+
+      if(!ticketNo || !title){
+        res.status(400).json({
+          error:true,
+          message:"please provide correct information 1"
+        })
+      }
+
+      const roleId = await UserRole.findOne({user_id:req.user._id}).select('role_id');
+      
+      if(!roleId || !roleId.role_id){
+          return res.status(401).json({
+                    error:true,
+                    message:"Unautherized user role.",
+                  })
+      }
+
+      if(roleId.role_id == process.env.ROLE_USER){
+          let ticketData = await Tickets.findOne({user_id:req.user._id,ticketNo,'status':'Payment Pending'});
+          let documentRequested = [...ticketData?.documentRequested];
+
+          if(documentRequested && Array.isArray(documentRequested)){
+              let currentDocumentIndex = documentRequested.findIndex(el => el.title == title)
+              if(currentDocumentIndex > -1){
+                let updatedDocument = {...documentRequested[currentDocumentIndex],document:req.files[0] }
+                documentRequested[currentDocumentIndex] = updatedDocument;
+                
+                const ticketDetails =  await Tickets.findOneAndUpdate(
+                  {user_id:req.user._id,ticketNo,'status':'Payment Pending'},
+                  { $set: {'documentRequested':documentRequested} },
+                  { new: true }
+                )
+      
+                return res.status(200).json(ticketDetails);
+              }
+              else{
+                return res.status(400).json({
+                  error:true,
+                  message:"please provide correct information 2"
+                })
+              }
+          }
+          else{
+            return res.status(400).json({
+              error:true,
+              message:"please provide correct information 3"
+            })
+          }
+      }
+      else{
+      return res.status(401).json({
+                error:true,
+                message:"Unautherized role.",
+              })
+      }
+    }
+    catch(error){
+      return res.status(500).json({
+        error:true,
+        message:"please provide correct information 4"
+      })
+    }
+}
