620389bb61ddae2adf349fabdd09fc4528d46f92
Sandeep Bisht <>
2024-01-29 18:47:03 +0530

    create update service api

--- controllers/management/services/index.js
+++ controllers/management/services/index.js
@@ -54,3 +54,109 @@ exports.createService = async (req, res) => {
         res.status(500).json({errorMessage:error.message,error:true, message: 'Internal server error' });
       }
 }
+
+exports.deleteServiceById = async (req, res) => {
+  try {
+    let {_id} = req.params._id;
+
+    let deleteService = await Services.findByIdAndDelete({_id});
+
+    if(!deleteService){
+      return res.status(404).json({
+        error: true,
+        message: 'Service not found.',
+      })
+    }
+
+    return res.status(200).json({
+      error : false,
+      message: 'Service deleted successfully'
+    })
+    
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: 'Error deleting Service.',
+    })
+  }
+}
+
+exports.updateServiceById = async (req, res) => {
+  
+    let {_id} = req.params;
+    let data = { ...req.body}
+    delete data.featuredImage;
+  delete data.featuredIcon;
+    
+    if(req.files){
+      try {
+        if (req.files &&  req.files.updatedFeaturedImage.length > 0) {
+          let featuredImage = req.files.updatedFeaturedImage.find((item) => item.fieldname == 'updatedFeaturedImage');
+          if (featuredImage){
+           
+            data = { ...data, featuredImage: featuredImage };
+          } 
+        }
+        if (req.files &&  req.files.updatedFeaturedIcon.length > 0) {
+          let featuredIcon = req.files.updatedFeaturedIcon.find((item) => item.fieldname == 'updatedFeaturedIcon');
+          if (featuredIcon){
+           
+            data = { ...data, featuredIcon: featuredIcon };
+          } 
+        }
+      } catch (uploadError) {
+        return res.status(500).json({
+          error: true,
+          message: "Error uploading files.",
+        });
+      }
+    }
+    try{
+      await Services.findByIdAndUpdate(_id,data).then((result)=>{
+          if(result){
+              res.status(200).json({
+                  error:false,
+                  message:"Service updated Successfully"
+              })
+          }else{
+              res.status(400).json({
+                  error:true,
+                  message:"Error updating Service"
+              })
+          }
+        })
+    }catch(error){
+       res.status(500).json({
+          error:true,
+          message:"Something went wrong, please try again later."
+       })
+    }
+
+  
+}
+
+exports.getServicesById = async (req, res) => {
+  try {
+    let {_id}  = req.params;
+
+    await Services.findById(_id).then(((result) =>{
+      if(result!==null) {
+        res.status(200).json({
+          error:false,
+           message:"No Data Found",
+           data:result
+        })
+      }else{
+        res.status(400).json({
+          error:true,
+          message:"Data not found"
+      })
+      }
+    }))
+  } catch (error) {
+    res.status(500).json({
+      error:true,
+      message:"Something went wrong"
+    })
+  }
+}
