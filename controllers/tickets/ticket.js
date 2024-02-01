6c8243d2d7565a70b1a192600e94dec59e0e57fa
Sandeep Bisht <>
2024-02-01 15:28:47 +0530

    update get all ticket

--- controllers/tickets/ticket.js
+++ controllers/tickets/ticket.js
@@ -10,6 +10,37 @@ const profile = require("../../models/profile");
 const { signature } = require("../../email/common");
 
 exports.getAllTickets = async (req, res) => {
+  try{
+    const roleId = await UserRole.findOne({user_id:req.user._id}).select('role_id');
+    
+    if(!roleId || !roleId.role_id){
+        return res.status(401).json({
+                  error:true,
+                  message:"Unautherized user role.",
+                })
+    }
+
+    if(roleId.role_id == process.env.ROLE_ADMIN){
+        let allTickets = await Tickets.find().populate('notice');
+        return res.status(200).json(allTickets);
+    }
+
+    if(roleId.role_id == process.env.ROLE_USER){
+        let allTickets = await Tickets.find({user_id:req.user._id,status:{ $ne: 'Paid' }}).populate('notice');
+        return res.status(200).json(allTickets);
+    }
+
+    return res.status(401).json({
+        error:true,
+        message:"Unautherized role.",
+      })
+  }
+  catch(error){
+    res.status(500).json({
+      error:true,
+      message:"please provide correct information"
+    })
+  }
 }
 
 exports.getByTicketNo = async (req, res) => {
