2162b10f6a2ac8237365fd2af518e10ec6563b9c
SHUBHAM-SEMWAL <87406338+SHUBHAM-SEMWAL@users.noreply.github.com>
2024-02-05 23:15:13 +0530

    remove auth from get all services api call

--- controllers/management/services/routes.js
+++ controllers/management/services/routes.js
@@ -4,7 +4,7 @@ const upload = require('../../../config/multer')
 const {createService, getAllServices, deleteServiceById, updateServiceById, getServicesById} = require("./index")
 const passport = require("passport")
 
-router.get('/get-all-services', passport.authenticate('jwt', {session:false}), getAllServices)
+router.get('/get-all-services', getAllServices)
 router.post('/create-service', passport.authenticate('jwt', {session:false}), upload.fields([{name: 'featuredImage'}, {name : 'featuredIcon'}]),createService);
 router.get('/get-service-by-id/:_id', passport.authenticate('jwt', {session:false}), getServicesById)
 router.delete('/delete-service-by-id/:_id', passport.authenticate('jwt', {session:false}), deleteServiceById);
